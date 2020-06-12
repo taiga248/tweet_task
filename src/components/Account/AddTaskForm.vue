6fb4ad7c65559c10a3292ccccb8440df419a7dcb
Taiga <taiga248@gmail.com>
2020-06-13 02:01:07 +0900

    Update AddTaskForm selecter

--- src/components/Account/AddTaskForm.vue
+++ src/components/Account/AddTaskForm.vue
@@ -3,13 +3,14 @@
     <v-card-text>
       <h2 class="text-center my-3">今日やったこと</h2>
       <v-form class="mx-5 my-10">
+        <!-- <v-select :items="tags" :v-model="tagChecked()" label="タグ"></v-select> -->
         <v-select :items="tags" label="タグ"></v-select>
         <p class="text-center">
           何時間作業した？
           <span class="title">- {{ working_time }}h -</span>
         </p>
         <v-slider v-model="working_time" max="12"></v-slider>
-        <v-textarea outlined :rules="textRules" label="内容"></v-textarea>
+        <v-textarea outlined v-model="text" :rules="textRules" label="内容"></v-textarea>
 
         <div class="text-center mb-10">
           <v-btn>キャンセル</v-btn>
@@ -24,14 +25,18 @@
 export default {
   data() {
     return {
-      tags: this.$parent.tags,
+      tags: ["仕事", "勉強", "課題"],
+      tagChecked: [],
       working_time: 4,
+      text: "",
       textRules: [v => !!v || "入力必須です"]
     };
   },
   methods: {
     save() {
-      console.log();
+      console.log(
+        this.tagChecked + " : " + this.working_time + " : " + this.text
+      );
     }
   }
 };
