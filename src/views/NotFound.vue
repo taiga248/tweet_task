a21ed586fca0b8c9684c14a56fe500519155ee5b
Taiga <taiga248@gmail.com>
2020-06-16 07:13:11 +0900

    Add: new file, 404page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto my-10 pa-10 text-center" width="500">
+      <p class="display-2">404 Not Found</p>
+      <v-card-text class="title">
+        Sorry... This page is nothing.
+        <br />Please move to another page.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" class="mx-auto mt-5" :to="{ name: 'Account'}">
+          <v-icon left>mdi-account</v-icon>
+          <span>アカウントページ</span>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
