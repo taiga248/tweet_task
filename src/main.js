ca7bc93cbe761cfcc3371c04734f0374450d82f6
Taiga <taiga248@gmail.com>
2020-07-16 02:31:56 +0900

    Change: firebase config place

--- src/main.js
+++ src/main.js
@@ -3,23 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import firebase from "firebase/app";
-import './registerServiceWorker'
-
+import "./plugins/firebase.config";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
-
-let firebaseConfig = {
-  apiKey: "AIzaSyCbrDRzob4gw1Jk8ZidSf_YNWBB3-AbaGQ",
-  authDomain: "tweet-task.firebaseapp.com",
-  databaseURL: "https://tweet-task.firebaseio.com",
-  projectId: "tweet-task",
-  storageBucket: "tweet-task.appspot.com",
-  messagingSenderId: "545490804922",
-  appId: "1:545490804922:web:62907f52b191267a8b2283"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
