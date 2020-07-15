4bc0998cb90801197a1a0e51da6a29122ab6b021
Taiga <taiga248@gmail.com>
2020-07-16 01:05:17 +0900

    #9 Add: PWA

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import firebase from "firebase/app";
+import './registerServiceWorker'
+
 
 Vue.config.productionTip = false;
 
