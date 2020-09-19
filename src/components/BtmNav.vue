6bd46b66d18ca1c045adf0bea76107d1d9617c9b
Taiga <taiga248@gmail.com>
2020-09-20 04:56:52 +0900

    #37 Add Bottom NaviBar

---
+++ src/components/BtmNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-bottom-navigation class="pt-2" :value="activeBtn" fixed color="#000">
+    <v-btn v-for="(item, index) in items" :key="index" :to="item.link">
+      <span>{{ item.title }}</span>
+      <v-icon>{{ item.icon }}</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeBtn: 1,
+      items: [
+        {
+          title: "アカウント",
+          icon: "mdi-account-circle-outline",
+          link: { name: "Account" }
+        },
+        {
+          title: "記録",
+          icon: "mdi-bulletin-board",
+          link: { name: "Recode" }
+        },
+        {
+          title: "みんなの記録",
+          icon: "mdi-content-save-all",
+          link: { name: "Allworks" }
+        },
+        {
+          title: "使い方",
+          icon: "mdi-mouse-variant",
+          link: { name: "Howto" }
+        }
+      ]
+    };
+  }
+};
+</script>
